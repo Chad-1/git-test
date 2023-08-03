701e1f3ab497e131ccbc7dcc74fb34898983b307
jiangchu <1140258980@qq.com>
2023-08-03 14:37:54 +0800

    arraylist.add

--- src/main/java/com/atguigu/git/GitTest02.java
+++ src/main/java/com/atguigu/git/GitTest02.java
@@ -10,5 +10,8 @@ import java.util.ArrayList;
 public class GitTest02 {
     public static void main(String[] args) {
         ArrayList arrayList = new ArrayList();
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
     }
 }
